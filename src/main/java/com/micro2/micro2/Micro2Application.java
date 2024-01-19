6bde8fc3108f43d102aa508f2ab2c20b716342e7
bengalurudev <bengalurudev23@gmail.com>
2024-01-20 00:29:14 +0530

    tomcat version changed to 9.0.4

--- src/main/java/com/micro2/micro2/Micro2Application.java
+++ src/main/java/com/micro2/micro2/Micro2Application.java
@@ -2,11 +2,15 @@ package com.micro2.micro2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Micro2Application {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args)
+    {
         SpringApplication.run(Micro2Application.class, args);
     }
 
